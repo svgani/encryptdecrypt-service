e91c96bb8dd6c704ac80b90b1f295c643910868f
Srinivasu Gannem <srinivasu.gannem@nxp.com>
2021-04-08 19:36:04 +0530

    added postmancollection and Generate key pair program

---
+++ src/main/java/com/example/encryptdecrypt/generatekeys/GenerateKeyPair.java
@@ -0,0 +1,28 @@
+package com.example.encryptdecrypt.generatekeys;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+public class GenerateKeyPair {
+    public static void main(String args[]) throws Exception{
+      //Creating KeyPair generator object
+      KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("DSA");
+
+      //Initializing the KeyPairGenerator
+      keyPairGen.initialize(2048);
+
+      //Generating the pair of keys
+      KeyPair pair = keyPairGen.generateKeyPair();
+
+      //Getting the private key from the key pair
+      PrivateKey privKey = pair.getPrivate();
+
+
+      //Getting the public key from the key pair
+      PublicKey publicKey = pair.getPublic();
+      System.out.println("#publicKey:"+publicKey.toString());
+      System.out.println("Keys generated");
+    }
+}
